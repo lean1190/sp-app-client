d3a13791e5edf7b7c3fac9ef22956048f1d7c965
Leandro <leito.vm3@hotmail.com>
2015-09-25 16:36:19 -0300

    First approach displaying the whole weekly schedule

--- www/js/week/WeekFindAllSchedulesController.js
+++ www/js/week/WeekFindAllSchedulesController.js
@@ -13,22 +13,35 @@
 
     function WeekFindAllSchedulesController(UserFactory) {
         var vm = this;
-        vm.schedules = [];
+        // Arreglo de dias
+        vm.schedules = {};
         vm.days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
-        vm.hours = ["8","9","11",];
+        vm.hours = [8, 9, 11];
+
+        for (var i = 0; i < vm.days.length; i++) {
+            var day = vm.days[i];
+            vm.schedules[day] = {};
+            for (var j = 0; j < vm.hours.length; j++) {
+                var hour = vm.hours[j];
+                vm.schedules[day][hour] = [];
+            }
+        }
 
         activate();
 
         function activate() {
-            return findAllUsers().then(function (users) {
-                console.log('--> Activate findAllUsers');
-            });
-        }
-
-        function findAllUsers() {
             return UserFactory.findAllUsers().then(function (users) {
-                return users;
-            }, function (err) {
+                users.forEach(function (currentUser) {
+                    for (var i = 0; i < currentUser.schedule.length; i++) {
+                        var scheduleEntry = currentUser.schedule[i];
+                        vm.schedules[scheduleEntry.day][scheduleEntry.startHour].push({
+                            name: currentUser.name,
+                            profilePhoto: currentUser.profilePhoto
+                        });
+                    }
+                });
+            },
+            function (err) {
                 console.log(err);
             });
         }
